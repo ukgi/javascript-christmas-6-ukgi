5c75773578da428674743f5367d690cdd9299c87
PARK-CHANUK <doosan1224@naver.com>
2023-11-10 19:46:34 +0900

    feat: 유저로부터 식당 예상 방문날짜를 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Console } from '@woowacourse/mission-utils';
+import InputView from './InputView.js';
+
 class App {
-  async run() {}
+  async run() {
+    const date = await this.#getDate();
+  }
+
+  async #getDate() {
+    try {
+      const answer = await InputView.readDate();
+      return answer;
+    } catch (error) {
+      Console.print(error.message);
+      return this.#getDate();
+    }
+  }
 }
 
 export default App;
--- src/InputView.js
+++ src/InputView.js
@@ -1,7 +1,16 @@
-export default InputView = {
-    async readDate() {
-        const input = await Console.readLineAsync("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        // ...
+import { Console } from '@woowacourse/mission-utils';
+
+const InputView = {
+  async readDate() {
+    const input = await Console.readLineAsync(
+      '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
+    );
+    if (Number.isNaN(Number(input)) || Number(input) < 1 || Number(input) > 31) {
+      throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
     }
-    // ...
-}
+    return Number(input);
+  },
+  // ...
+};
+
+export default InputView;
