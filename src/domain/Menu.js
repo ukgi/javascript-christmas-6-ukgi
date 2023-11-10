11979a20926cdb1226aa7b1f376d29c12f67f9c3
PARK-CHANUK <doosan1224@naver.com>
2023-11-10 20:12:37 +0900

    feat(Menu): 모든 메뉴명을 반환하는 기능 구현

---
+++ src/domain/Menu.js
@@ -0,0 +1,37 @@
+export default class Menu {
+  static #menu = {
+    애피타이저: {
+      양송이수프: 6_000,
+      타파스: 5_500,
+      시저샐러드: 8_000,
+    },
+    메인: {
+      티본스테이크: 55_000,
+      바비큐립: 54_000,
+      해산물파스타: 35_000,
+      크리스마스파스타: 25_000,
+    },
+    디저트: {
+      초코케이크: 15_000,
+      아이스크림: 5_000,
+    },
+    음료: {
+      제로콜라: 3_000,
+      레드와인: 60_000,
+      샴페인: 25_000,
+    },
+  };
+
+  static getOnlyMenuTitle() {
+    const menuTitles = [];
+
+    Object.keys(this.#menu).forEach((category) => {
+      const menuItems = this.#menu[category];
+      Object.keys(menuItems).forEach((menuItem) => {
+        menuTitles.push(menuItem);
+      });
+    });
+
+    return menuTitles;
+  }
+}
