73b710a87e61b1a1c97aca04e4ad9303d28aa386
PARK-CHANUK <doosan1224@naver.com>
2023-11-10 20:50:06 +0900

    test(MenuTest): 테스트케이스 추가 case2: 카테고리를 입력하면 해당 카테고리의 메뉴를 반환합니다.

--- __tests__/domain/MenuTest.js
+++ __tests__/domain/MenuTest.js
@@ -17,4 +17,25 @@ describe('메뉴 테스트', () => {
       '샴페인',
     ]);
   });
+
+  test.each([
+    {
+      category: '애피타이저',
+      expected: ['양송이수프', '타파스', '시저샐러드'],
+    },
+    {
+      category: '메인',
+      expected: ['티본스테이크', '바비큐립', '해산물파스타', '크리스마스파스타'],
+    },
+    {
+      category: '디저트',
+      expected: ['초코케이크', '아이스크림'],
+    },
+    {
+      category: '음료',
+      expected: ['제로콜라', '레드와인', '샴페인'],
+    },
+  ])('카테고리를 입력하면 해당 카테고리의 메뉴를 반환합니다.', ({ category, expected }) => {
+    expect(Menu.getMenuByCategory(category)).toEqual(expected);
+  });
 });
