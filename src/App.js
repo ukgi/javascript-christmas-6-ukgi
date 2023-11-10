9552c1a0ddfd1692f702e59450c2b7c654b0dfa9
PARK-CHANUK <doosan1224@naver.com>
2023-11-10 21:12:34 +0900

    feat: 주문할 메뉴와 갯수를 입력받고 유효성 검사 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Console } from '@woowacourse/mission-utils';
 import InputView from './InputView.js';
+import MenuOrder from './domain/MenuOrder.js';
 
 class App {
   async run() {
@@ -13,6 +14,16 @@ class App {
     } catch (error) {
       Console.print(error.message);
       return this.#getDate();
+    }
+  }
+
+  async #getOrder() {
+    try {
+      const answer = await InputView.readOrder();
+      new MenuOrder(answer);
+    } catch (error) {
+      Console.print(error.message);
+      return this.#getDate;
     }
   }
 }
---
+++ src/domain/MenuOrder.js
@@ -0,0 +1,50 @@
+import Menu from './Menu';
+
+export default class MenuOrder {
+  #allOrders;
+
+  constructor(order) {
+    const orderMenuTitle = order.map(([title]) => title.replace(/\s/g, ''));
+    const orderMenuCount = order.map(([, count]) => Number(count));
+    this.#validate(orderMenuTitle, orderMenuCount);
+    this.#allOrders = order.map(([menu, count]) => [menu.replace(/\s/g, ''), Number(count)]);
+  }
+
+  #validate(orderMenuTitle, orderMenuCount) {
+    if (
+      !this.#isAvailable(orderMenuTitle) ||
+      this.#isDuplicate(orderMenuTitle) ||
+      !this.#isValidOrderQuantity(orderMenuCount)
+    ) {
+      throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
+    }
+    if (this.#isOnlyOrderBeverage(orderMenuTitle)) {
+      throw new Error('[ERROR] 음료만 주문할 수 없습니다. 다시 입력해 주세요.');
+    }
+    if (this.#isExceedOrderLimit(orderMenuCount)) {
+      throw new Error('[ERROR] 한번에 주문할 수 있는 갯수는 최대 20개입니다. 다시 입력해 주세요.');
+    }
+  }
+
+  #isAvailable(orderMenuTitle) {
+    const allMenuTitle = Menu.getOnlyMenuTitle();
+    return orderMenuTitle.every((orderMenu) => allMenuTitle.includes(orderMenu));
+  }
+
+  #isDuplicate(orderMenuTitle) {
+    return orderMenuTitle.length !== new Set(orderMenuTitle).size;
+  }
+
+  #isOnlyOrderBeverage(orderMenuTitle) {
+    const beverages = Menu.getMenuByCategory('음료');
+    return orderMenuTitle.every((menu) => beverages.includes(menu));
+  }
+
+  #isValidOrderQuantity(orderMenuCount) {
+    return orderMenuCount.every((count) => count >= 1);
+  }
+
+  #isExceedOrderLimit(orderMenuCount) {
+    return orderMenuCount.reduce((acc, cur) => (acc += cur), 0) > 20;
+  }
+}
