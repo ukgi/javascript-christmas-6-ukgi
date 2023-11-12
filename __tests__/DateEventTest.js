030c6a82244de1819e4a7d4b80b1147eca61c584
PARK-CHANUK <doosan1224@naver.com>
2023-11-12 15:24:15 +0900

    test(DateEventTest): 테스트 케이스 추가 case7: 할인이 적용되지 않으면 빈 배열 반환

--- __tests__/DateEventTest.js
+++ __tests__/DateEventTest.js
@@ -9,6 +9,7 @@ describe('방문날짜 이벤트 테스트', () => {
       ['초코케이크', 1],
     ];
     const dateEvent = new DateEvent(date, order);
+
     expect(dateEvent.getBenefit()).toEqual([
       ['크리스마스 디데이 할인', 1300],
       ['평일 할인', 2023],
@@ -23,6 +24,7 @@ describe('방문날짜 이벤트 테스트', () => {
       ['초코케이크', 1],
     ];
     const dateEvent = new DateEvent(date, order);
+
     expect(dateEvent.getBenefit()).toEqual([
       ['크리스마스 디데이 할인', 3400],
       ['평일 할인', 2023],
@@ -38,6 +40,7 @@ describe('방문날짜 이벤트 테스트', () => {
       ['초코케이크', 1],
     ];
     const dateEvent = new DateEvent(date, order);
+
     expect(dateEvent.getBenefit()).toEqual([
       ['크리스마스 디데이 할인', 1000],
       ['주말 할인', 2023],
@@ -52,6 +55,7 @@ describe('방문날짜 이벤트 테스트', () => {
       ['초코케이크', 1],
     ];
     const dateEvent = new DateEvent(date, order);
+
     expect(dateEvent.getBenefit()).toEqual([['평일 할인', 2023]]);
   });
 
@@ -63,6 +67,7 @@ describe('방문날짜 이벤트 테스트', () => {
       ['초코케이크', 1],
     ];
     const dateEvent = new DateEvent(date, order);
+
     expect(dateEvent.getBenefit()).toEqual([
       ['평일 할인', 2023],
       ['특별 할인', 1000],
@@ -77,7 +82,19 @@ describe('방문날짜 이벤트 테스트', () => {
       ['초코케이크', 1],
     ];
     const dateEvent = new DateEvent(date, order);
+
     expect(dateEvent.getBenefit()).toEqual([['주말 할인', 2023]]);
+  });
+
+  test('할인이 적용되지 않으면 빈 배열을 반환', () => {
+    const date = 29;
+    const order = [
+      ['타파스', 1],
+      ['시저샐러드', 1],
+    ];
+    const dateEvent = new DateEvent(date, order);
+
+    expect(dateEvent.getBenefit()).toEqual([]);
   });
 });
 
@@ -90,6 +107,7 @@ describe('총 할인혜택 금액 테스트', () => {
       ['초코케이크', 1],
     ];
     const dateEvent = new DateEvent(date, order);
+
     expect(dateEvent.getTotalDiscount()).toBe(3323);
   });
 });
