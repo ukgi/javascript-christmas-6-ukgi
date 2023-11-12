03f752adc482145ac615ce86e21471606c81f4b4
PARK-CHANUK <doosan1224@naver.com>
2023-11-13 01:22:06 +0900

    test(EventHandlerManagerTest): 테스트 코드 구현 test: 이벤트 혜택내역 반환 테스트 case1: 이벤트에 따른 혜택내역을 반환한다.

---
+++ __tests__/EventHandlerManagerTest.js
@@ -0,0 +1,18 @@
+import EventHandlerManager from '../src/EventHandlerManager';
+
+describe('이벤트 혜택내역 반환 테스트', () => {
+  test('이벤트에 따른 혜택내역을 반환합니다.', () => {
+    const date = 4;
+    const order = [
+      ['해산물파스타', 1],
+      ['레드와인', 1],
+      ['초코케이크', 1],
+    ];
+    const event = ['크리스마스 디데이 할인', '평일 할인'];
+
+    expect(EventHandlerManager.getBenefitByDate(date, order, event)).toEqual([
+      ['크리스마스 디데이 할인', 1300],
+      ['평일 할인', 2023],
+    ]);
+  });
+});
