a43e3ecc88c5cd31f43463e44d2d44340e36d33c
PARK-CHANUK <doosan1224@naver.com>
2023-11-13 22:16:43 +0900

    test(EventBadgeFinderTest): 테스트코드 구현 test: 총 혜택금액에 따른 이벤트 뱃지 테스트 case1: 총 혜택금액이 5000원 미만이면 뱃지 없음 case2: 총 혜택금액이 5000원 이상 10,000원 미만이면 별 뱃지 case3: 총 혜택금액이 10,000원 이상 20,000원 미만이면 트리 뱃지 case4: 총 혜택금액이 20,000원 이상이면 산타 뱃지

---
+++ __tests__/EventBadgeFinderTest.js
@@ -0,0 +1,26 @@
+import EventBadgeFinder from '../src/EventBadgeFinder';
+
+describe('뱃지 테스트', () => {
+  test('총 혜택금액이 5000원 미만이면 뱃지 없음', () => {
+    const totalDiscount = 4999;
+    expect(EventBadgeFinder.findBadge(totalDiscount)).toBe('없음');
+  });
+
+  test.each([5000, 9999])(
+    '총 혜택금액이 5000원이상 10,000원 미만이면 별 뱃지 수여',
+    (totalDiscount) => {
+      expect(EventBadgeFinder.findBadge(totalDiscount)).toBe('별');
+    },
+  );
+
+  test.each([10_000, 19_999])(
+    '총 혜택금액이 10,000원이상 20,000원 미만이면 트리 뱃지 수여',
+    (totalDiscount) => {
+      expect(EventBadgeFinder.findBadge(totalDiscount)).toBe('트리');
+    },
+  );
+
+  test.each([20_000, 30_000])('총 혜택금액이 20_000원 이상이면 산타 뱃지 수여', (totalDiscount) => {
+    expect(EventBadgeFinder.findBadge(totalDiscount)).toBe('산타');
+  });
+});
