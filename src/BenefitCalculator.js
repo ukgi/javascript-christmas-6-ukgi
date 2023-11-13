e0b4ae3eafcfb88126253d7e644a7bb643b5c3a0
PARK-CHANUK <doosan1224@naver.com>
2023-11-13 22:33:43 +0900

    refactor: 이벤트에 따른 혜택내역 반환 기능을 BenefitCalculator 내부에서 수행하도록 수정 EventDiscountor를 삭제하고 해당 기능을 BenefitCalculator 내부로 이동

--- src/BenefitCalculator.js
+++ src/BenefitCalculator.js
@@ -1,9 +1,7 @@
 import EventFinder from './EventFinder.js';
-import EventDiscountor from './EventDiscountor.js';
 import MenuManager from './lib/MenuManager.js';
 
 export default class BenefitCalculator {
-  // 총 혜택내역을 계산해서 반환하는 외부인터페이스
   calculateTotalBenefit(date, menu, amount) {
     if (amount >= 10_000) {
       const totalBenefit = this.#getTotalBenefit(date, menu, amount);
@@ -17,7 +15,7 @@ export default class BenefitCalculator {
     const totalBenefit = [];
     if (amount >= 120_000) totalBenefit.push(['증정 이벤트', 25_000]);
     const event = EventFinder.getEventByDate(date);
-    const benefitByDate = EventDiscountor.calculateBenefitByDate(date, menu, event);
+    const benefitByDate = this.#calculateBenefitByDate(date, menu, event);
     benefitByDate.forEach((benefit) => {
       totalBenefit.push(benefit);
     });
@@ -30,5 +28,39 @@ export default class BenefitCalculator {
       totalDiscount += amount;
     });
     return totalDiscount;
+  }
+
+  #calculateBenefitByDate(date, menu, event) {
+    const discount = [];
+    event.forEach((eventName) => {
+      if (eventName === '크리스마스 디데이 할인') discount.push(this.#christmasDdayHandler(date));
+      if (eventName === '평일 할인') discount.push(this.#menuEventHandler(eventName, menu));
+      if (eventName === '주말 할인') discount.push(this.#menuEventHandler(eventName, menu));
+      if (eventName === '특별 할인') discount.push(this.#specialDayHandler());
+    });
+    return discount.filter(([, discountAmount]) => discountAmount > 0);
+  }
+
+  #christmasDdayHandler(date) {
+    return ['크리스마스 디데이 할인', 1000 + 100 * date - 100];
+  }
+
+  #menuEventHandler(eventName, menu) {
+    let discount = 0;
+    if (eventName === '평일 할인') {
+      menu.forEach(([menuName, count]) => {
+        if (MenuManager.getCategoryByMenu(menuName) === '디저트') discount += 2023 * count;
+      });
+    }
+    if (eventName === '주말 할인') {
+      menu.forEach(([menuName, count]) => {
+        if (MenuManager.getCategoryByMenu(menuName) === '메인') discount += 2023 * count;
+      });
+    }
+    return [eventName, discount];
+  }
+
+  #specialDayHandler() {
+    return ['특별 할인', 1000];
   }
 }
