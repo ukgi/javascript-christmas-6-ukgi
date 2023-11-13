660fea71c7fc48b1ad8e1984c4acdafd6ef6ff64
PARK-CHANUK <doosan1224@naver.com>
2023-11-13 22:39:47 +0900

    refactor(EventFinderTest): 테스트 케이스 수정

--- __tests__/EventFinderTest.js
+++ __tests__/EventFinderTest.js
@@ -1,9 +1,32 @@
 import EventFinder from '../src/EventFinder';
 
 describe('이벤트 리스트 반환 테스트', () => {
-  test('날짜를 전달하면 해당하는 이벤트 반환', () => {
-    const date = 4;
-
-    expect(EventFinder.getEventByDate(date)).toEqual(['크리스마스 디데이 할인', '평일 할인']);
+  test.each([
+    {
+      date: 4,
+      event: ['크리스마스 디데이 할인', '평일 할인'],
+    },
+    {
+      date: 25,
+      event: ['크리스마스 디데이 할인', '평일 할인', '특별 할인'],
+    },
+    {
+      date: 1,
+      event: ['크리스마스 디데이 할인', '주말 할인'],
+    },
+    {
+      date: 26,
+      event: ['평일 할인'],
+    },
+    {
+      date: 31,
+      event: ['평일 할인', '특별 할인'],
+    },
+    {
+      date: 29,
+      event: ['주말 할인'],
+    },
+  ])('날짜를 전달하면 해당하는 이벤트 반환', ({ date, event }) => {
+    expect(EventFinder.getEventByDate(date)).toEqual(event);
   });
 });
