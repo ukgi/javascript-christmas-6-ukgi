cf025a73d7eb48300e9db9539fa611a8203b8546
PARK-CHANUK <doosan1224@naver.com>
2023-11-14 00:44:38 +0900

    refactor(BenefitCalculator): 기능 중복 제거

--- src/BenefitCalculator.js
+++ src/BenefitCalculator.js
@@ -4,21 +4,12 @@ import MenuManager from './lib/MenuManager.js';
 export default class BenefitCalculator {
   calculateTotalBenefit(date, menu, amount) {
     if (amount >= 10_000) {
-      const totalBenefit = this.#getTotalBenefit(date, menu, amount);
+      const event = EventFinder.getEvent(date, amount);
+      const totalBenefit = this.#calculateBenefit(date, menu, event);
       const totalDiscount = this.#getTotalDiscount(totalBenefit);
       return { totalBenefit, totalDiscount };
     }
     return { totalBenefit: [], totalDiscount: 0 };
-  }
-
-  #getTotalBenefit(date, menu, amount) {
-    const totalBenefit = [];
-    const event = EventFinder.getEvent(date, amount);
-    const benefitByDate = this.#calculateBenefitByDate(date, menu, event);
-    benefitByDate.forEach((benefit) => {
-      totalBenefit.push(benefit);
-    });
-    return totalBenefit;
   }
 
   #getTotalDiscount(totalBenefit) {
@@ -29,18 +20,18 @@ export default class BenefitCalculator {
     return totalDiscount;
   }
 
-  #calculateBenefitByDate(date, menu, event) {
-    const discount = [];
+  #calculateBenefit(date, menu, event) {
+    const benefit = [];
     event.forEach((eventName) => {
       if (eventName === '크리스마스 디데이 할인')
-        discount.push(this.#christmasDdayHandler(eventName, date));
-      if (eventName === '평일 할인') discount.push(this.#menuEventHandler(eventName, menu));
-      if (eventName === '주말 할인') discount.push(this.#menuEventHandler(eventName, menu));
-      if (eventName === '특별 할인') discount.push(this.#specialDayHandler(eventName));
-      if (eventName === '증정 이벤트') discount.push(this.#giftHandler(eventName));
+        benefit.push(this.#christmasDdayHandler(eventName, date));
+      if (eventName === '평일 할인') benefit.push(this.#menuEventHandler(eventName, menu));
+      if (eventName === '주말 할인') benefit.push(this.#menuEventHandler(eventName, menu));
+      if (eventName === '특별 할인') benefit.push(this.#specialDayHandler(eventName));
+      if (eventName === '증정 이벤트') benefit.push(this.#giftHandler(eventName));
     });
 
-    return discount.filter(([, discountAmount]) => discountAmount > 0);
+    return benefit.filter(([, discountAmount]) => discountAmount > 0);
   }
 
   #christmasDdayHandler(eventName, date) {
